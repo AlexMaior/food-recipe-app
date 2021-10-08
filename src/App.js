a50759103c968ef3d3dfee88f85d217f02521c29
Alex Maior <alexmaior@yahoo.com>
2021-10-08 04:59:14 +0300

    small change

--- src/App.js
+++ src/App.js
@@ -18,18 +18,15 @@ function App() {
   const foodInputRef = useRef();
 
   useEffect(() => {
+    const getRecipes = async () => {
+      const response = await fetch(
+        `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+      );
+      const data = await response.json();
+      setRecipes(data.hits);
+    };
     getRecipes();
   }, [query]);
-
-  const getRecipes = async () => {
-    const response = await fetch(
-      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
-    );
-    const data = await response.json();
-    setRecipes(data.hits);
-    console.log(data.hits);
-    console.log(data);
-  };
 
   const updateSearch = (e) => {
     if (e.target.value.trim().length > 0) {
